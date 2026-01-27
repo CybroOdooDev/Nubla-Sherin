6f1dcf1a382e1c6929f3c4683cb511385b38fcd9
nubla <nublasherin61@gmail.com>
2026-01-27 20:13:03 +0530

    removing vertical line from saleorder report

---
+++ custom_sale_order_report/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cats and Dogs Solution
+#
+#    Copyright (C) Cats and Dogs Solution.
+#
+#    This program is under the terms of the Odoo Proprietary License v1.0
+#    (OPL-1)
+#    It is forbidden to publish, distribute, sublicense, or sell copies of the
+#    Software or modified copies of the Software.
+#
+################################################################################
+{
+    'name': "SaleOrder Report",
+    'version': '18.0.1.0.0',
+    'depends': ['sale'],
+    'summary': 'Custom SaleOrder report',
+    'author': 'Cats and Dogs Solutions',
+    'company': 'Cats and Dogs Solutions',
+    'maintainer': 'Cats and Dogs Solutions',
+    'data': [
+        'views/sale_report.xml',
+    ],
+
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
