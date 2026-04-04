55c0717ff1e7d6c60b28241f616f5249b39badc2
nubla <nublasherin61@gmail.com>
2026-04-04 20:05:47 +0530

    created module for pos font adjusting

---
+++ pos_font_size/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'POS Font Size',
+    'version': '19.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Configure Font Sizes for POS UI Elements',
+    'description': """This module allows you to configure font sizes for product names, 
+    prices, order lines, and receipts in the Point of Sale.""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_config_views.xml',
+    ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_font_size/static/src/js/**/*',
+            'pos_font_size/static/src/css/**/*',
+            'pos_font_size/static/src/xml/**/*',
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
