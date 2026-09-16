f2b8d6451e585f3872e799fd54bd37bd019f3456
nubla <nublasherin61@gmail.com>
2026-09-16 17:08:25 +0530

    created new module for manging NHS theme

---
+++ NHS/theme_nhs/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    "name": "NHS Theme",
+    "version": "19.0.1.0.0",
+    "category": "Themes/Backend",
+    "summary": "NHS Backend Theme is the official theme for NHS modules in Odoo",
+    "description": """Minimalist and elegant backend theme tailored for NHS Trust Management and other NHS apps""",
+    "author": "Cybrosys Techno Solutions",
+    "company": "Cybrosys Techno Solutions",
+    "maintainer": "Cybrosys Techno Solutions",
+    "website": "https://www.cybrosys.com",
+    "depends": ["web", "mail"],
+    "data": [
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "theme_nhs/static/src/xml/menu_panels.xml",
+            "theme_nhs/static/src/xml/nav_bar_panel.xml",
+            "theme_nhs/static/src/xml/home_menus.xml",
+            "theme_nhs/static/src/xml/side_bar_panel.xml",
+            "theme_nhs/static/src/scss/nav.scss",
+            "theme_nhs/static/src/scss/sidebar.scss",
+            "theme_nhs/static/src/css/style.css",
+            "theme_nhs/static/src/js/home_menus.js",
+            "theme_nhs/static/src/js/search_apps.js",
+        ],
+        "web.assets_frontend": [
+            "theme_nhs/static/src/scss/login.scss",
+        ],
+    },
+    "images": [
+        "static/description/banner.jpg",
+        "static/description/theme_screenshot.jpg",
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
---
+++ NHS/theme_nhs/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <nhs>
+#### 21.02.2026
+#### Version 19.0.1.0.0
+##### ADD
+
+- Initial commit for NHS Theme  
+
+
